8c50a61962bea1320226a0679efc7d3978bbfcc0
Cartus <1002562@c049114.wlan.net.ed.ac.uk>
2019-12-15 17:35:20 +0000

    add pretrained model

--- semeval/eval.py
+++ semeval/eval.py
@@ -13,7 +13,7 @@ from utils.vocab import Vocab
 
 parser = argparse.ArgumentParser()
 parser.add_argument('model_dir', type=str, help='Directory of the model.')
-parser.add_argument('--model', type=str, default='best_model.pt', help='Name of the model file.')
+parser.add_argument('--model', type=str, default='checkpoint_epoch_150.pt', help='Name of the model file.')
 parser.add_argument('--data_dir', type=str, default='dataset/tacred')
 parser.add_argument('--dataset', type=str, default='test', help="Evaluate on dev or test.")
 
